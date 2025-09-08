64496cd0b931df2f3e5e75d80badd1c5ee8aa313
Alex Carpenter <alpentater@gmail.com>
2025-09-08 15:49:42 -0500

    Initial commit

---
+++ CTA1/CTA1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    //Printing a first name, last name, street address, city and zip code for a fictional person.
+    std::cout << "First Name: Jim\n";
+    std::cout << "Last Name: Jimberson\n";
+    std::cout << "Street Adress: 100 Cabana Cay Cir\n";
+    std::cout << "City: Panama City\n";
+    std::cout << "Zip Code: 32405\n";
+
+    //Waiting for output screen
+    std::cin.get();
+       
+    //Main return statement.
+    return 0;
+}
